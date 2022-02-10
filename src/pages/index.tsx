34e035afad2eff7b9eacc84188ffc5e8d25e50c1
Guilherme Vieira <hello@guilhermevieira.dev>
2022-02-10 14:59:17 -0300

    feat: create header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ignews.</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
